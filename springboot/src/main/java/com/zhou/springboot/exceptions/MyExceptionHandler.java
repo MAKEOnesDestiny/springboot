d7bb92d180e7a2cd64d1e8d1bd4bb0f2a32e6881
hfzhou <hfzhou@leqee.com>
2020-06-13 18:07:27 +0800

    工具类提交

--- springboot/src/main/java/com/zhou/springboot/exceptions/MyExceptionHandler.java
+++ springboot/src/main/java/com/zhou/springboot/exceptions/MyExceptionHandler.java
@@ -35,7 +35,6 @@ public class MyExceptionHandler{
     private JSONObject analysisBody(HttpServletRequest request){
         String str = getBodyFromRequest(request);
         if(StringUtils.isEmpty(str)){
-            log.error("传入body的字符串为空");
             return null;
         }
         return JSONObject.parseObject(str);
