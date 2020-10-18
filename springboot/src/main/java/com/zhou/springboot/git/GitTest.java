3b6436e2a3b58e0c0e4edef0f4d6ea45f0e56fca
hfzhou <m13588034977@163.com>
2020-10-18 22:33:16 +0800

    测试

--- springboot/src/main/java/com/zhou/springboot/git/GitTest.java
+++ springboot/src/main/java/com/zhou/springboot/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("我是第一次提交的git");
         System.out.println("我是第三次提交的git");
         System.out.println("测试本地超前远程");
+        System.out.println("新提交的内容");
     }
 
 }
